d7cbd54fa4926200e4b56c9f9285c457e49021c6
luizfiller <luiz.fsilva@sptech.school>
2023-08-01 14:34:44 -0300

    Add - métodos da service Unique-Id

---
+++ src/app/shared/services/unique-id/unique-id.service.ts
@@ -0,0 +1,28 @@
+import { Injectable } from "@angular/core";
+import { v4 as uuidV4 } from "uuid";
+
+@Injectable()
+
+export class UniqueIdService {
+    private numberOfGeneratedIds = 0;
+    
+    public generateUniqueIdWithPrefix(prefix: string): string {
+        if (!prefix) {
+            throw Error("Prefix can not be empty");
+        }
+        
+        const uniqueId = this.generateUniqueId();
+
+        this.numberOfGeneratedIds++;
+
+        return `${prefix}-${uniqueId}`;
+    }
+
+    public getNumberOfGeneratedUniqueIds(): number {
+        return this.numberOfGeneratedIds;
+    }
+
+    private generateUniqueId(): string {
+        return uuidV4();
+    }
+}
