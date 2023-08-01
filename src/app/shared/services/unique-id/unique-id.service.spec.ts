db6479341e6669eed18b15bea444f3cbeb8f17cb
luizfiller <luiz.fsilva@sptech.school>
2023-08-01 14:35:14 -0300

    [WIP -Add] - estrutura de UnitTest

---
+++ src/app/shared/services/unique-id/unique-id.service.spec.ts
@@ -0,0 +1,15 @@
+import {  } from "module";
+
+describe('O artefato que queremos testar', () => {
+    it('Primeira condição que queremos testar', () => {
+        // Preparação
+        // Execução
+        // Verificação
+    });
+
+    it('Segunda condição que queremos testar', () => {
+        // Preparação
+        // Execução
+        // Verificação
+    });
+});
