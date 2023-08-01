283ff9205293b1889962085538044d8317ba9ed9
luizfiller <luiz.fsilva@sptech.school>
2023-08-01 12:46:37 -0300

    Update - angular cli e core p/ @12 e mudança nome do projeto

--- karma.conf.js
+++ karma.conf.js
@@ -16,7 +16,7 @@ module.exports = function (config) {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/ng-test1'),
+      dir: require('path').join(__dirname, './coverage/angular-test-training'),
       subdir: '.',
       reporters: [
         { type: 'html' },
