1942bf2a30e54fb0379a78a15d97fe8e897a8a75
luizfiller <luiz.fsilva@sptech.school>
2023-08-04 17:49:39 -0300

    Add - beforeEach w/ TestBed, import Module, specs/it(), spyOn

--- src/app/shared/components/like-widget/like-widget.component.spec.ts
+++ src/app/shared/components/like-widget/like-widget.component.spec.ts
@@ -1,25 +1,50 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { TestBed, ComponentFixture, ComponentFixtureAutoDetect } from '@angular/core/testing';
 
-import { LikeWidgetComponent } from './like-widget.component';
+import { LikeWidgetComponent } from "./like-widget.component";
+import { LikeWidgetModule } from './like-widget.module';
 
-describe('LikeWidgetComponent', () => {
-  let component: LikeWidgetComponent;
-  let fixture: ComponentFixture<LikeWidgetComponent>;
+describe(LikeWidgetComponent.name, () => {
+    let fixture: ComponentFixture<LikeWidgetComponent> = null;
+    let component: LikeWidgetComponent = null;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ LikeWidgetComponent ]
+    beforeEach( async () => {
+        await TestBed.configureTestingModule({
+            imports: [LikeWidgetModule],
+            providers: [
+                {
+                    provide: ComponentFixtureAutoDetect,
+                    useValue: true
+                }
+            ]
+        }).compileComponents();
+
+        fixture = TestBed.createComponent(LikeWidgetComponent);
+        component = fixture.componentInstance;
     })
-    .compileComponents();
-  });
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(LikeWidgetComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+    it(`#should create component`, () => {
+        expect(component).toBeTruthy();
+    })
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
-});
+    it(`#${LikeWidgetComponent.prototype.ngOnInit.name} 
+        should automatically generate an ID when (@Input id) is not assigned`, () => {
+        expect(component.id).toBeTruthy();
+    })
+
+    it(`#${LikeWidgetComponent.prototype.ngOnInit.name} 
+        should NOT automatically generate an ID when (@Input id) is assinged`, () => {
+        const someId = 'someId';
+
+        component.id = someId;
+
+        expect(component.id).toBe('someId');
+    })
+
+    it(`#${LikeWidgetComponent.prototype.like.name}
+        should trigger (@Output liked) when called`, () => {
+        const spy = spyOn(component.liked, 'emit');
+        
+        component.like();
+        expect(spy).toHaveBeenCalled();
+    })
+})
--- src/app/shared/components/like-widget/like-widget.module.ts
+++ src/app/shared/components/like-widget/like-widget.module.ts
@@ -2,6 +2,7 @@ import { CommonModule } from "@angular/common";
 import { NgModule } from "@angular/core";
 import { LikeWidgetComponent } from "./like-widget.component";
 import { FontAwesomeModule } from "@fortawesome/angular-fontawesome";
+import { UniqueIdService } from "../../services/unique-id/unique-id.service";
 
 @NgModule({
     declarations: [
@@ -14,6 +15,9 @@ import { FontAwesomeModule } from "@fortawesome/angular-fontawesome";
     exports: [
         LikeWidgetComponent
     ],
+    providers: [
+        UniqueIdService
+    ]
 })
 
 export class LikeWidgetModule { }
